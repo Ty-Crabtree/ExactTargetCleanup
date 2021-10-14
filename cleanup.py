23adfd4d04bd09c1676217a693d5c3a360c56d66
ty-crabtree <tyler.crabtree@smarsh.com>
2021-10-14 13:55:20 -0700

    add logging + log

--- cleanup.py
+++ cleanup.py
@@ -3,7 +3,7 @@
 import os
 import glob
 from csv import reader
-
+import logging
 
 class CLeanup:
 
@@ -11,11 +11,17 @@ class CLeanup:
         self.base_path = '/mnt/datastorage/ftphome/vanguardetproduction'
         self.csv_list = []
         self.files_to_search = []
+        self.logger = logging.getLogger('Cleanup')
+        self.logger.setLevel(logging.DEBUG)
+        # create file handler which logs even debug messages
+        fh = logging.FileHandler('cleanup.log')
+        fh.setLevel(logging.DEBUG)
+        self.logger.addHandler(fh)
 
     def get_files_from_directory(self, test_path=None):
         if test_path:
             self.base_path = test_path
-        self.files_to_search = glob.glob(f"{self.base_path}/*")
+        self.files_to_search = glob.glob(self.base_path+'/*')
 
     def get_csv_files(self):
         with open('cleanup.csv', 'r') as read_obj:
@@ -26,17 +32,24 @@ class CLeanup:
     def delete_files(self):
         for file in self.files_to_search:
             for csv_file in self.csv_list:
-                print(f"{file} comparing to {csv_file}")
+                #self.logger.debug(file+" comparing to "+csv_file)
+                #print(file+" comparing to "+csv_file)
                 if csv_file in file:
-                    print(f"Removing {file}")
+                    self.logger.debug("Removing "+file)
+                    print("Removing "+file)
                     try:
-                        os.remove(file)
+                        pass
+                        #os.remove(file)
                     except Exception as e:
+                        self.logger.debug("Couldn't remove file!")
+                        self.logger.debug(e)
+
                         print("Couldn't remove file!")
                         print(e)
 
     def controller(self):
-        print(f"Starting Cleanup on {self.base_path}")
+        print("Starting Cleanup on "+self.base_path)
+        self.logger.debug("Starting Cleanup on "+self.base_path)
         self.get_files_from_directory()
         self.get_csv_files()
         self.delete_files()
